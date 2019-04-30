c17bb0356a2082a150c1f6d9fd0aeb8cdd6b48bd
nemurin <yh.kyud.28@gmail.com>
2019-05-01 04:01:24 +0900

    コントローラをヘッダーやフッターに対応させた

--- rails/RailsApp/app/controllers/helo_controller.rb
+++ rails/RailsApp/app/controllers/helo_controller.rb
@@ -1,18 +1,9 @@
 class HeloController < ApplicationController
+    layout 'helo'
     def index
-        if request.post? then
-            @title='Result'
-            if params['s1'] then
-            @msg = 'You selected: '
-            for val in params['s1']
-                @msg += val + ' '
-            end
-            else
-                @msg ='not selected...'
-            end
-        else 
-            @title='Index'
-            @msg='select radio button...'
-        end
+        @header = 'layout sample'
+        @footer = 'copyright SYODA-Tuyano 2016.'
+        @title = 'New Layout'
+        @msg='this is sample page!'
     end
 end
