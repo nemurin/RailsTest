24dc28513e6877e7a7107b233dfdf421fd60d4d5
nemurin <yh.kyud.28@gmail.com>
2019-05-01 01:31:05 +0900

    GETとPOSTの処理分岐を作成

--- rails/RailsApp/app/controllers/helo_controller.rb
+++ rails/RailsApp/app/controllers/helo_controller.rb
@@ -1,13 +1,13 @@
 class HeloController < ApplicationController
     def index
-        if params['msg']!=nil then
-            @title=params['msg']
+        if request.post? then
+            @title='Result'
+            @msg = 'You typed:'+params['input1']+'.'
+            @value=params['input1']
         else 
-            @title='index'
+            @title='Index'
+            @msg='type text'
+            @value=''
         end
-        @msg='this is redirect sample...'
-    end
-    def other
-        redirect_to action: :index,params:{'msg':'from other page'}
     end
 end
