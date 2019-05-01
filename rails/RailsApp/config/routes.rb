8239f99cf707b1ddac348c84825b849f7c89e88f
nemurin <yh.kyud.28@gmail.com>
2019-05-02 02:19:23 +0900

    ルーティング設定

--- rails/RailsApp/config/routes.rb
+++ rails/RailsApp/config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   get 'people/edit/:id',to: 'people#edit'
   post 'people/edit/:id',to: 'people#update'
   patch 'people/edit/:id', to:'people#update'
+  get 'people/delete/:id', to:'people#delete'
 end
