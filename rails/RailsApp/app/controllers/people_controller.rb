aa2d37116619d2f2e27a4112eb48935743ef6173
nemurin <yh.kyud.28@gmail.com>
2019-05-02 00:51:07 +0900

    コントローラにaddとcreateを追加

--- rails/RailsApp/app/controllers/people_controller.rb
+++ rails/RailsApp/app/controllers/people_controller.rb
@@ -7,4 +7,17 @@ class PeopleController < ApplicationController
     @msg="Index data."
     @data=Person.find(params[:id])
   end
+  def add
+    @msg="add new data."
+  end
+  protect_from_forgery
+  def create
+    if request.post? then
+      obj=Person.create(
+        name:params['name']
+        age:params['age']
+        mail:params['mail']
+      )
+      end
+      redirect_to '/people'
 end
