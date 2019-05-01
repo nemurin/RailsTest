f815f5703eb5d6ec5d64b7572665b95fa374a6be
nemurin <yh.kyud.28@gmail.com>
2019-05-01 15:42:19 +0900

    コントローラを実装

--- rails/RailsApp/app/controllers/people_controller.rb
+++ rails/RailsApp/app/controllers/people_controller.rb
@@ -3,4 +3,8 @@ class PeopleController < ApplicationController
     @msg='Person data.'
     @data= Person.all
   end
+  def show
+    @msg="Index data."
+    @data=Person.find(params[:id])
+  end
 end
