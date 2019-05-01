87f8fe1e8e3805e6e9c79d0ea8b8d1dfecef84d8
nemurin <yh.kyud.28@gmail.com>
2019-05-02 01:22:26 +0900

    コントローラの修正

--- rails/RailsApp/app/controllers/people_controller.rb
+++ rails/RailsApp/app/controllers/people_controller.rb
@@ -9,16 +9,15 @@ class PeopleController < ApplicationController
   end
   def add
     @msg="add new data."
+    @person =Person.new
   end
-  protect_from_forgery
   def create
     if request.post? then
-      obj=Person.create(
-        name:params['name'],
-        age:params['age'],
-        mail:params['mail'],
-      )
+      Person.create(person_params)
       end
       redirect_to '/people'
     end
+  private
+  def person_params
+    params.require(:person).permit(:name,:age,:mail)
 end
