c3e1a8670f917ec24fe97e8bdef21762f1dc5fca
nemurin <yh.kyud.28@gmail.com>
2019-05-03 03:35:22 +0900

    メールアドレスのバリデーション文章を実装

--- rails/RailsApp/app/models/person.rb
+++ rails/RailsApp/app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
     validates :name, presence: {message:'は必須項目です'}
     validates :age, numericality: {message:'は、数字で入力ください'}
+    validates :mail, email: {message:'はメールアドレスではありません'}
 end
