e91b5428da637478acde3e4f6d2b5aa49c20d050
nemurin <yh.kyud.28@gmail.com>
2019-05-02 17:08:38 +0900

    コントローラにfindを実装

--- rails/RailsApp/app/controllers/people_controller.rb
+++ rails/RailsApp/app/controllers/people_controller.rb
@@ -32,6 +32,14 @@ class PeopleController < ApplicationController
     obj.destroy
     redirect_to 'people'
   end
+  def find
+    @msg='please type search word...'
+    @people=Array.new
+    if request.post? then
+      obj=Person.find params['find']
+      @people.push obj
+    end
+  end
   private
   def person_params
     params.require(:person).permit(:name,:age,:mail)
