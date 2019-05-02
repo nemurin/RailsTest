a0649821368a7376b8a01dcb12164a18a7468de9
nemurin <yh.kyud.28@gmail.com>
2019-05-02 20:46:35 +0900

    入力に問題がある場合の分岐を作成

--- rails/RailsApp/app/controllers/people_controller.rb
+++ rails/RailsApp/app/controllers/people_controller.rb
@@ -13,10 +13,11 @@ class PeopleController < ApplicationController
     @person =Person.new
   end
   def create
-    if request.post? then
-      Person.create(person_params)
-      end
+    @person =Person.new person_params
+    if @person.save then
       redirect_to '/people'
+    else
+      @msg='入力に問題があります'
   end
   def edit
     @msg="edit data.[id="+params[:id]+"]"
