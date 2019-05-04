cca6f4a29574ef82c6c6ac06ca878ba5203fce79
nemurin <yh.kyud.28@gmail.com>
2019-05-04 11:32:49 +0900

    プリコンパイル設定

--- rails/RailsApp/config/initializers/assets.rb
+++ rails/RailsApp/config/initializers/assets.rb
@@ -14,3 +14,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( people.css )
 Rails.application.config.assets.precompile += %w( people.js )
+
+Rails.application.config.assets.precompile += %w( messages.css )
+Rails.application.config.assets.precompile += %w( messages.js )
