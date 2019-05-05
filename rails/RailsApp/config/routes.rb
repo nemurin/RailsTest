0269e7b7e8c25564255b0c42504d67a146873f7b
nemurin <yh.kyud.28@gmail.com>
2019-05-05 19:59:10 +0900

    controllerをinitial commit

--- rails/RailsApp/config/routes.rb
+++ rails/RailsApp/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'blogposts/index'
+  get 'blogposts/add'
+  get 'blogposts/edit'
+  get 'blogposts/delete'
+  get 'bloggenres/index'
+  get 'bloggenres/add'
+  get 'bloggenres/edit'
+  get 'blogconfigs/index'
+  get 'blogconfigs/edit'
   get 'blogs/index'
   get 'blogs',to:'blogs#index'
   get 'blogs/:page',to:'blogs#index'
