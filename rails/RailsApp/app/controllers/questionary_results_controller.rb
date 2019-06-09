4158d193d09769696abdee6fa429a60b61d460f1
nemurin <yh.kyud.28@gmail.com>
2019-06-09 19:48:09 +0900

    コントローラにCalc関数実装

--- rails/RailsApp/app/controllers/questionary_results_controller.rb
+++ rails/RailsApp/app/controllers/questionary_results_controller.rb
@@ -73,4 +73,25 @@ class QuestionaryResultsController < ApplicationController
     def questionary_result_params
       params.require(:questionary_result).permit(:questionary_id, :result)
     end
+
+    def calc
+      @questionary = Questionary.find params[:id]
+      results = QuestionaryResult.where('questionary_id = ?',params[:id])
+      @calc ={}
+      results.each do |result|
+        data = result.result.split ','
+        data.each do |value|
+          keyval = value.split ':'
+          ky = keyval[0].to_s
+          vl = keyval[1].to_i
+          if ky != 'question_id' then
+            if @calc[ky] == nil then
+              @calc[ky] = []
+            end
+            @calc[ky][vl] = @calc[ky][vl] == nil ? 1 :
+              @calc[ky][vl].to_i + 1
+          end
+        end
+      end
+    end
 end
